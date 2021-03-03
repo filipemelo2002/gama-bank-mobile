aba41cf4cce9a6d92b515edb5b386a6644e0dfd7
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-03-03 13:20:28 -0300

    RecoveryPassword APi integration

--- src/screens/RecoveryPassword/index.tsx
+++ src/screens/RecoveryPassword/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from 'react';
-import { StatusBar } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { ActivityIndicator, StatusBar } from 'react-native';
 import { KeyboardAvoidingView, ScrollView, Platform, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { 
@@ -16,13 +17,69 @@ import {
   LinkText } from './style';
 
 import logoGamaBank from '../../images/gamabank.png';
+import api from '../../services/api';
 
 const RecoveryPassword: React.FC = () => {
   const navigator = useNavigation();
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  const [confirmPassword,setConfirmPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = useCallback(() => {
-    navigator.navigate('signin');
-  }, []);
+  async function handleSubmit() {
+    setIsLoading(true);
+
+    if (username === '' || password === '') {
+      Toast.show({
+        type: 'error',
+        text1: '',
+        text2: 'Preencha corretamente os campos',
+        topOffset: 60,
+      });
+
+      setIsLoading(false);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      Toast.show({
+        type: 'error',
+        text1: '',
+        text2: 'Senhas não coincidem.',
+        topOffset: 60,
+      });
+      
+      setIsLoading(false);
+      return;
+    }
+
+    const postDataNovaSenha = {
+      email: 'fake@mail.com',
+      login: username,
+    };
+
+    const postDataAlteraSenha = {
+      senha: password,
+      usuario: username,
+    };
+
+    try{
+      const response = await api.post('nova-senha', postDataNovaSenha);
+      await api.post(`altera-senha?senhaTemporaria=${response.data}`, postDataAlteraSenha)
+      setUsername('');
+      setPassword('');
+      setConfirmPassword('');
+      setIsLoading(false);
+      navigator.navigate('signin');
+    }catch(err){
+      Toast.show({
+        type: 'error',
+        text1: '',
+        text2: 'Ocorreu um erro tente novamente mais tarde',
+      });
+      setIsLoading(false);
+    }
+  };
 
   const handleGoBack = useCallback(() => {
     navigator.navigate('signin');
@@ -47,17 +104,41 @@ const RecoveryPassword: React.FC = () => {
             <Content>
               <Title>Redefinir senha</Title>
               <InputContainer>
-                <Input placeholder="Nome de usuário" />
+                <Input 
+                  placeholder="Nome de usuário"
+                  autoCapitalize="none"
+                  value={username}
+                  onChangeText={text => setUsername(text)}
+                />
               </InputContainer>
               <InputContainer>
-                <Input placeholder="Nova senha" />
+                <Input 
+                  placeholder="Nova senha"
+                  secureTextEntry={true}
+                  autoCapitalize="none"
+                  value={password}
+                  onChangeText={text => setPassword(text)}
+                />
               </InputContainer>
               <InputContainer>
-                <Input placeholder="Confirmar nova senha" />
+                <Input 
+                  placeholder="Confirmar nova senha"
+                  secureTextEntry={true}
+                  autoCapitalize="none"
+                  value={confirmPassword}
+                  onChangeText={text => setConfirmPassword(text)}
+                />
               </InputContainer>
-              <Button onPress={handleSubmit}>
-                <ButtonText>Continuar</ButtonText>
-                <Icon name="arrow-right" size={24} color="#FFF" />
+              <Button onPress={handleSubmit} disabled={isLoading}>
+                {isLoading 
+                  ? (<ActivityIndicator size="small" color="#8C52E5" style={{flex: 1, alignSelf: 'center'}} />) 
+                  : (
+                    <>
+                      <ButtonText>Continuar</ButtonText>
+                      <Icon name="arrow-right" size={24} color="#FFF" />
+                    </>
+                    )
+                }
               </Button>
               <Link onPress={handleGoBack}>
                 <Icon name="chevron-left" size={12} color="#8C52E5" />
@@ -70,6 +151,7 @@ const RecoveryPassword: React.FC = () => {
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
     </>
   );
 }
